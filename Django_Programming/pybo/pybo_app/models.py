58716482613b638c436863c1d5c3ea43767250b7
Shineast <canfrog@naver.com>
2026-04-25 17:26:46 +0900

    [pybo] Change Model

--- Django_Programming/pybo/pybo_app/models.py
+++ Django_Programming/pybo/pybo_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
@@ -8,7 +10,8 @@ class Question(models.Model):
     def __str__(self):
         return self.subject
 
-class Answer(models.Model):
+class Answer(models.Model): 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
