0f0d3a411e7bcaee347bf404cdc2be0f9dcb522b
Shineast <canfrog@naver.com>
2026-04-28 14:42:37 +0900

    [pybo] Markdown

--- Django_Programming/pybo/pybo_app/templatetags/pybo_filter.py
+++ Django_Programming/pybo/pybo_app/templatetags/pybo_filter.py
@@ -1,7 +1,17 @@
+import markdown
+import nh3
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter
 def sub(value, arg):
     return value - arg
+
+@register.filter
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    html = markdown.markdown(value, extensions=extensions)
+    clean_html = nh3.clean(html)
+    return mark_safe(clean_html)
