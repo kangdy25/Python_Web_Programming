32d3a681920e78509c51aa6e0abce300d0d4c323
Shineast <canfrog@naver.com>
2026-06-17 10:57:59 +0900

    [pytodo] my first django-rest-framework api

--- Django_Programming/pytodo/pytodo_app/config/settings.py
+++ Django_Programming/pytodo/pytodo_app/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'example'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
