50e72a089dcb2894719d5dab064689d46525658d
Shineast <canfrog@naver.com>
2026-06-18 13:52:14 +0900

    [pytodo] Mixins, Generics, ViewSet, Router

--- Django_Programming/pytodo/pytodo_app/example/urls.py
+++ Django_Programming/pytodo/pytodo_app/example/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path, include
-from .views import HelloAPI, bookAPI, booksAPI
+from .views import BookAPI, BooksAPI, BooksAPIMixins, BookAPIMixins, BookViewSet
+from rest_framework import routers
 
-urlpatterns = [
-    path("hello/", HelloAPI),
-    path("fbv/books/", booksAPI),
-    path("fbv/book/<int:bid>/", bookAPI)
-]
+router = routers.SimpleRouter()
+router.register('books', BookViewSet)
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path("cbv/books/", BooksAPI.as_view()),
+#     path("cbv/book/<int:bid>/", BookAPI.as_view()),
+#     path("mixin/books/", BooksAPIMixins.as_view()),
+#     path("mixin/book/<int:bid>/", BookAPIMixins.as_view())
+# ]
