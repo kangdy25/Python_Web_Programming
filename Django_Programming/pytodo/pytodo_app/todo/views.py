e8f0025d076bd444363de674fb0d025a23e38488
Shineast <canfrog@naver.com>
2026-06-19 18:25:32 +0900

    [pytodo] create todo view all API

--- Django_Programming/pytodo/pytodo_app/todo/views.py
+++ Django_Programming/pytodo/pytodo_app/todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import viewsets
+
+from .models import Todo
+from .serializers import TodoSimpleSerializer
+
+class TodosAPIView(APIView):
+    def get(self, request):
+        todos = Todo.objects.filter(complete=False)
+        serializer = TodoSimpleSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
