bf052fb5616144272b4f5097042ffe22e7ef76cc
Shineast <canfrog@naver.com>
2026-06-22 14:51:06 +0900

    [pyboard] implement sign-up

--- Django_Programming/pyboard/be/config/settings.py
+++ Django_Programming/pyboard/be/config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -98,6 +100,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
 
 
 # Internationalization
