79b7b50905f6972ab4928b65722683940bbb9011
Shineast <canfrog@naver.com>
2026-06-24 18:42:18 +0900

    [pyboard] (be) implement create posts

--- Django_Programming/pyboard/be/config/settings.py
+++ Django_Programming/pyboard/be/config/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'users',
-    'corsheaders'
+    'posts'
 ]
 
 MIDDLEWARE = [
