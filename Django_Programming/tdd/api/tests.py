9ba701e8a41a85965e74fa03e6385cb668c56920
Shineast <canfrog@naver.com>
2026-07-01 13:02:33 +0900

    [tdd] unit test about models

--- Django_Programming/tdd/api/tests.py
+++ Django_Programming/tdd/api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Book
 
-# Create your tests here.
+from rest_framework.test import APIClient
+from rest_framework import status
+
+class ModelTest(TestCase):
+    def setUp(self):
+        self.book_title = "My Book"
+        self.book_author = "Issac"
+        self.book = Book(title=self.book_title, author=self.book_author)
+
+    def test_model_can_create_a_bucketlist(self):
+        old_count = Book.objects.count()
+        self.book.save()
+        new_count = Book.objects.count()
+        self.assertNotEqual(old_count, new_count)
